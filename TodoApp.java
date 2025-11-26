a109d8ef38332b8bf5af0f8467d65cd13af94d3a
Manav9547 <manavjindal000u8@gmail.com>
2025-11-26 13:33:56 +0530

    Update TodoApp.java

--- TodoApp.java
+++ TodoApp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class TodoApp{
 
-    private static ArrayList<String> todoList = new ArrayList<>();
+    public static ArrayList<String> todoList = new ArrayList<>();
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
